be4f9c5b0dca1c49579b8bc1e8d2e6c5ed4da01b
Yasheed1995 <qwerty8608301@gmail.com>
2017-11-21 15:34:19 +0800

    finish train.py

--- hw4/load_data.py
+++ hw4/load_data.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 __author__ = 'b04901025'
 # File script.py
 import os
@@ -14,51 +13,51 @@ import h5py
 VALIDATION_SPLIT = 0.2
 
 if __name__ == '__main__':
-	buffer_ = []
-	texts = []
-	labels = []
-	texts_labels = {}
-	with open('data/training_label.txt', 'r') as f:
-		buffer_ = f.read()
-		print len(buffer_.split('\n'))
-		for line in buffer_.split('\n'):
-			if line == "":
-				break
-			texts.append(line[10:])
-			labels.append(line[0])
-		#print np.array(texts)
-		#print np.array(labels)
-	tokenizer = Tokenizer(10000)
-	tokenizer.fit_on_texts(texts)
-	sequences = tokenizer.texts_to_sequences(texts)
-	
-	word_index = tokenizer.word_index
-	print('Found %s unique tokens.' % len(word_index))
-	
-	data = pad_sequences(sequences, maxlen=1000)
-	
-	labels = np_utils.to_categorical(np.asarray(labels))
-	print('Shape of data tensor:', data.shape)
-	print('Shape of label tensor:', labels.shape)
-	
-	indices = np.arange(data.shape[0])
-	np.random.shuffle(indices)
-	data = data[indices]
-	labels = labels[indices]
-	nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
-	
-	X_train = data[:-nb_validation_samples]
-	y_train = labels[:-nb_validation_samples]
-	X_valid = data[-nb_validation_samples:]
-	y_valid = labels[-nb_validation_samples:]
-	
-	print len(X_train)
-	print len(y_train)
-	print len(X_valid)
-	print len(y_valid)
-	with h5py.File('data/data.h5', 'w') as hf:
-		hf.create_dataset('X_train', data=X_train)
-		hf.create_dataset('y_train', data=y_train)
-		hf.create_dataset('X_valid', data=X_valid)
-		hf.create_dataset('y_valid', data=y_valid)
-		hf.close()
+    buffer_ = []
+    texts = []
+    labels = []
+    texts_labels = {}
+    with open('data/training_label.txt', 'r') as f:
+        buffer_ = f.read()
+        print len(buffer_.split('\n'))
+        for line in buffer_.split('\n'):
+            if line == "":
+                break
+            texts.append(line[10:])
+            labels.append(line[0])
+        #print np.array(texts)
+        #print np.array(labels)
+    tokenizer = Tokenizer(10000)
+    tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+
+    word_index = tokenizer.word_index
+    print('Found %s unique tokens.' % len(word_index))
+
+    data = pad_sequences(sequences, maxlen=1000)
+
+    labels = np_utils.to_categorical(np.asarray(labels))
+    print('Shape of data tensor:', data.shape)
+    print('Shape of label tensor:', labels.shape)
+
+    indices = np.arange(data.shape[0])
+    np.random.shuffle(indices)
+    data = data[indices]
+    labels = labels[indices]
+    nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+    X_train = data[:-nb_validation_samples]
+    y_train = labels[:-nb_validation_samples]
+    X_valid = data[-nb_validation_samples:]
+    y_valid = labels[-nb_validation_samples:]
+
+    print len(X_train)
+    print len(y_train)
+    print len(X_valid)
+    print len(y_valid)
+    with h5py.File('data/data.h5', 'w') as hf:
+        hf.create_dataset('X_train', data=X_train)
+        hf.create_dataset('y_train', data=y_train)
+        hf.create_dataset('X_valid', data=X_valid)
+        hf.create_dataset('y_valid', data=y_valid)
+        hf.close()
--- hw4/train.py
+++ hw4/train.py
@@ -1,7 +1,115 @@
-#!/usr/bin/env python
-__author__ = 'Yasheed'
-# File script.py
+# -*- coding: utf-8 -*-
+__author__ = 'b04901025'
+# File train.py
+import tensorflow   as tf
+import numpy        as np
+import os
+import argparse
+import keras
+import h5py
+from keras.models               import Sequential, load_model
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers           import Adam, Adadelta
+from keras.utils                import np_utils
+from keras                      import regularizers
+from keras.preprocessing.image  import ImageDataGenerator
+from keras.layers               import Dense, Dropout, Activation, Flatten
+from keras.layers               import Convolution2D, Conv2D, AveragePooling2D
+from keras.layers               import ZeroPadding2D, MaxPooling2D
+from keras.layers.embeddings    import Embedding
+from keras.callbacks            import ModelCheckpoint,EarlyStopping
+
+def main():
+    parser = argparse.ArgumentParser(prog='train.py')
+    parser.add_argument('--epoch', type=int, default=30)
+    parser.add_argument('--batch', type=int, default=512)
+    parser.add_argument('--pretrain', type=bool, default=False)
+    parser.add_argument('--model_name', type=str, default='save/model/model-1')
+    parser.add_argument('--which_model', type=int, default=0)
+    parser.add_argument('--which_gpu', type=int, default=0)
+    parser.add_argument('--gpu_fraction', type=float, default=1.0)
+
+    args = parser.parse_args()
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.which_gpu)
+
+    # gpu limitation
+    from keras.backend.tensorflow_backend import set_session
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = args.gpu_fraction
+    set_session(tf.Session(config=config))
+
+    (X_train, y_train), (X_valid, y_valid) = load_data()
+
+    train(
+        args.which_model,
+        X_train,
+        y_train,
+        X_valid,
+        y_valid,
+        args.batch,
+        args.epoch,
+        args.pretrain,
+        args.model_name
+    )
+
+
+def load_data():
+    with h5py.File('data/data.h5', 'r') as hf:
+        X_train = hf['X_train'][:]
+        y_train = hf['y_train'][:]
+        X_valid = hf['X_valid'][:]
+        y_valid = hf['y_valid'][:]
+
+    return (X_train, y_train), (X_valid, y_valid)
+
+def train(which_model, X_train, y_train, X_valid, y_valid,
+         n_batch, n_epoch, pretrain, model_name):
+
+    if pretrain == False:
+        if which_model == 0:
+            model = build_model_0()
+        #elif which_model == 1:
+        #    model = build_model_1()
+        #elif which_model == 2:
+        #    model = build_model_2()
+    else:
+        model = load_model(model_name)
+
+    filepath='save/Model.{epoch:02d}-{val_acc:.4f}.hdf5'
+    checkpoint1 = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    checkpoint2 = EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=0, mode='auto')
+    callbacks_list = [checkpoint1,checkpoint2]
+
+    '''
+    model.fit_generator(datagen_train.flow(X_train, y_train,
+                    batch_size=n_batch),
+                    epochs=n_epoch,
+                    steps_per_epoch=len(X_train)/n_batch,
+                    validation_data=datagen_test.flow(X_valid, y_valid,
+                        batch_size=n_batch),
+                    validation_steps=len(X_valid)/n_batch,
+                    samples_per_epoch=X_train.shape[0],
+                    callbacks=callbacks_list)
+    '''
+
+    model.fit(X_train, y_train, batch_size=n_batch, epochs=n_epoch, callbacks=callbacks_list)
+    model.save('save/model/' + str(model_name) + '.hdf5')
+
+    score = model.evaluate(X_valid, y_valid)
+    print ('\nTest Acc:', score[1])
+
+def build_model_0():
+    model = Sequential()
+    model.add(Embedding(160000, 64, 1000))
+    model.add(Flatten())
+    model.add(Dense(64, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    return model
 
 
 if __name__ == '__main__':
-	print
+    main()
