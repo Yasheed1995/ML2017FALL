41db2c9daf33014b694a91853f46e69d91d9eb71
Yasheed1995 <qwerty8608301@gmail.com>
2017-11-21 16:23:32 +0800

    add model 1

--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -3,3 +3,6 @@
 python3.6 load_data.py 
 python3.6 train.py --epoch 30 --batch 128 --model_name \
     1 --which_gpu 0 --gpu_fraction 1
+
+python3.6 train.py --epoch 30 --batch 128 --model_name \
+    1 --which_gpu 0 --gpu_fraction 1 --which_model 1
