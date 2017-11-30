b67b35a6ca52fdd601d9af3a815aa3f32de37f17
Yasheed1995 <qwerty8608301@gmail.com>
2017-11-30 11:48:37 +0800

    change sh

--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash 
 
 #python3.6 load_data.py 
-python3.6 train.py --epoch 30 --batch 256 --model_name \
-    4 --which_gpu 0 --gpu_fraction 1 --which_model 2
+python3.6 train.py --epoch 5 --batch 1024 --model_name \
+    6 --which_gpu 0 --gpu_fraction 0.9 --which_model 2
 
-python3.6 train.py --epoch 30 --batch 512 --model_name \
-    5 --which_gpu 0 --gpu_fraction 1 --which_model 2
+python3.6 train.py --epoch 5 --batch 1024 --model_name \
+    7 --which_gpu 0 --gpu_fraction 0.9 --which_model 1
